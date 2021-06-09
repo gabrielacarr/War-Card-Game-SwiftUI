8b3e69772146584cdd9cd4e9645ead053a8bb23a
gabriela <24256452+gabrielacarr@users.noreply.github.com>
2021-06-09 16:25:37 -0400

    Initial Commit

---
+++ War Card Game UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  War Card Game UI
+//
+//  Created by Brie New on 6/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War Card Game UI/War_Card_Game_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_Game_UIApp.swift
+//  War Card Game UI
+//
+//  Created by Brie New on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_Game_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
