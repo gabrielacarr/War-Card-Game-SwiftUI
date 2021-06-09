ddf17419e1116faa9c67e59e4e282e9fa905e2ca
gabriela <24256452+gabrielacarr@users.noreply.github.com>
2021-06-09 17:24:37 -0400

    Update: Style + Spacing for Stacks

--- War Card Game UI/ContentView.swift
+++ War Card Game UI/ContentView.swift
@@ -7,15 +7,66 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        ZStack {
+            Image("background").ignoresSafeArea()
+
+
+        // logo
+        VStack() {
+            Spacer()
+            Image("logo").padding([.top])
+            Spacer()
+                // card stack
+                    HStack() {
+                        Spacer()
+                        Image("back")
+                        Spacer()
+                        Image("back")
+                        Spacer()
+                }
+                    Spacer()
+                    // deal button
+                        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image("dealbutton")
+                }
+                    Spacer()
+                   // player stack
+                    HStack(alignment: .center){
+                        Spacer()
+                        VStack() {
+                            Text(/*@START_MENU_TOKEN@*/"Player"/*@END_MENU_TOKEN@*/)
+                                .font(.headline)
+                                .foregroundColor(Color.white)
+                                .padding(.bottom, 2.0)
+                            Text("0")
+                                .font(.largeTitle)
+                                .foregroundColor(Color.white)
+                        }
+                        Spacer()
+                        VStack() {
+                            Text("CPU")
+                                .font(.headline)
+                                .foregroundColor(Color.white)
+                                .padding(.bottom, 1.0)
+                            Text("0")
+                                .font(.largeTitle)
+                                .foregroundColor(Color.white)
+                        }
+                        Spacer()
+                    }
+            Spacer()
+         }
+      }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+
     }
+}
 }
